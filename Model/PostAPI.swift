25a5c16ce08a052668717eb051818a31e5185999
Lawrence <lawrencehan94@gmail.com>
2017-09-18 15:42:46 -0700

    v2

--- Model/PostAPI.swift
+++ Model/PostAPI.swift
@@ -1,9 +1,15 @@
 import Foundation
 import Firebase
 
-//Start by creating a reference to the Firebase Database's child folder (sub-folder) called "Posts"
 
 class PostAPI {
+  
+  // Start by creating a reference to the Firebase Database's child folder (sub-folder) called "Posts"
   var REF_POSTS = Database.database().reference().child("posts")
+  
+  // Then get a snapshot of the post data (in the form of a dictionary), which we can use our transformPost function to turn it into a Post Class
+  func observePosts(completion: @escaping (Post) -> Void) {
+    REF_POSTS.observe(.childAdded) {
+  }
   
 }
