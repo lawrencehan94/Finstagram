7a0f595e6ad2fcb83e591dcb446ce3ac3e9aca58
Lawrence <lawrencehan94@gmail.com>
2017-09-18 15:11:48 -0700

    v3

--- Model/Post.swift
+++ Model/Post.swift
@@ -13,3 +13,22 @@ class Post {
   var caption: String?
   var isLiked: Bool?
 }
+
+// Turn the post class into a dictionary
+// For example, a post by Bob playing basketball would have a key (230asdfs082) as a unique identifier for the post, and then would have a ID: bobmarley32, a caption: "Watch me play ball", a photoURL: "fdsadsfd.png", a likeCount: 3, etc.
+// We are setting the properties of our post equal to the dictionary values inputted by a function (likely from Firebase)
+
+extension Post {
+  static func transformPost(postDictionary: [String, Any], key: String) -> Post {
+    
+    let post = Post()
+    
+    post.id = key
+    post.uid = postDictionary["uid"] as? String
+    post.photoURL = postDictionary["photoURL"] as? String
+    post.likecount = postDictionary["likeCount"] as? String
+    post.likes = postDictionary["likes"] as? Dictionary<String, Any>
+    post.caption = postDictionary["caption"] as? String
+    post isLiked = postDictionary["isLiked"] as? Bool
+    
+  }
