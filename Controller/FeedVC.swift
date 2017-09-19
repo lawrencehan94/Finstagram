35919123c8b5036b338c0f7f9a0f0a91e2503d4c
Lawrence <lawrencehan94@gmail.com>
2017-09-18 18:56:03 -0700

    v2

--- Controller/FeedVC.swift
+++ Controller/FeedVC.swift
@@ -1,1 +1,25 @@
+import UIKit
 
+class FeedVC: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  var posts = [Post]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+}
+
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return posts.count 
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+  }
+    
+}
